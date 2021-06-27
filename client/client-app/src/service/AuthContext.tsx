59306b1b91f22f9813fcb7f609c1a4c46240edf1
y-kim15 <yik_kk18@outlook.com>
2021-06-27 14:29:54 +0100

    using context to capture executions on load

---
+++ client/client-app/src/service/AuthContext.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const AuthContext = React.createContext<ContextType | null>(null);
+
+// export function useAuth() {
+//     return useContext(AuthContext);
+// }
